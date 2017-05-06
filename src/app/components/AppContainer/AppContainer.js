4b4c8af8117305725ed58b126c3f91b6fff920f9
Marcel Sebald <blazing-star@hotmail.de>
2017-05-06 22:36:20 +0200

    Spaces to Tabs

--- src/app/components/AppContainer/AppContainer.js
+++ src/app/components/AppContainer/AppContainer.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Layout, Row, Col, BackTop, Button} from 'antd';
+import { connect } from 'react-redux';
+import { Layout, Row, Col, BackTop, Button } from 'antd';
 import SVGInline from 'react-svg-inline';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 import logo from '../../assets/logo.svg';
 import * as firebase from 'firebase';
 
@@ -15,73 +15,73 @@ import Login from '../Login/Login.js';
 
 import styles from './AppContainer.scss';
 
-const {Header, Footer, Sider, Content} = Layout;
+const { Header, Footer, Sider, Content } = Layout;
 
 class AppContainer extends React.Component {
-  static propTypes = {
-    dispatch: React.PropTypes.func,
-    loggedIn: React.PropTypes.bool,
-  };
+	static propTypes = {
+		dispatch: React.PropTypes.func,
+		loggedIn: React.PropTypes.bool,
+	};
 
-  render() {
-    return firebase.auth().currentUser
-      ? <Layout className={styles.layout}>
-          <Header className={styles.header}>
-            <Row type="flex" justify="start">
-              <Col span="8">
-                <div className={styles.brand}><SVGInline svg={logo} /></div>
-              </Col>
-              <Col span="16" className={styles.rightHeaderColumn}>
-                <Login renderLogout />
-              </Col>
-            </Row>
-          </Header>
-          <hr />
-          <Layout className={styles.contentLayout}>
-            <Sider className={styles.sider}>
-              <div className={styles.sidebarWrapper}>
-                <SystemSelect />
-                <hr />
-                <Filter />
-              </div>
-            </Sider>
-            <Content className={styles.content}>
-              <Row>
-                <Col span={24} className={styles.contentColSpacing}>
-                  <AddGame buttonTitle="Add Game" />
-                  <AddSystem />
-                  <Link to="statistics">
-                    <Button type="primary" icon="bar-chart">Statistics</Button>
-                  </Link>
-                  <div className={styles.key}>🕹 = playing | ✅ = finished</div>
-                </Col>
-              </Row>
-              <GamesTable />
-            </Content>
-          </Layout>
-          <Footer className={styles.footer}>
-            <Row>
-              <Col span={6} className={styles.footerLeft}>
-                FireGames | {new Date().getFullYear()}
-              </Col>
-              <Col span={12} />
-              <Col span={6} className={styles.footerRight}>
-                v1.0
-              </Col>
-            </Row>
-          </Footer>
-          <BackTop />
-        </Layout>
-      : <Login />;
-  }
+	render() {
+		return firebase.auth().currentUser
+			? <Layout className={styles.layout}>
+					<Header className={styles.header}>
+						<Row type="flex" justify="start">
+							<Col span="8">
+								<div className={styles.brand}><SVGInline svg={logo} /></div>
+							</Col>
+							<Col span="16" className={styles.rightHeaderColumn}>
+								<Login renderLogout />
+							</Col>
+						</Row>
+					</Header>
+					<hr />
+					<Layout className={styles.contentLayout}>
+						<Sider className={styles.sider}>
+							<div className={styles.sidebarWrapper}>
+								<SystemSelect />
+								<hr />
+								<Filter />
+							</div>
+						</Sider>
+						<Content className={styles.content}>
+							<Row>
+								<Col span={24} className={styles.contentColSpacing}>
+									<AddGame buttonTitle="Add Game" />
+									<AddSystem />
+									<Link to="statistics">
+										<Button type="primary" icon="bar-chart">Statistics</Button>
+									</Link>
+									<div className={styles.key}>🕹 = playing | ✅ = finished</div>
+								</Col>
+							</Row>
+							<GamesTable />
+						</Content>
+					</Layout>
+					<Footer className={styles.footer}>
+						<Row>
+							<Col span={6} className={styles.footerLeft}>
+								FireGames | {new Date().getFullYear()}
+							</Col>
+							<Col span={12} />
+							<Col span={6} className={styles.footerRight}>
+								v1.0
+							</Col>
+						</Row>
+					</Footer>
+					<BackTop />
+				</Layout>
+			: <Login />;
+	}
 }
 
 let component = AppContainer;
 
 const mapStateToProps = state => {
-  return {
-    ...state,
-  };
+	return {
+		...state,
+	};
 };
 
 component = connect(mapStateToProps)(component);
