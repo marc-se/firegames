95efed57e4ea78b0170dc1a6f6a05b5915dd0456
Marcel Sebald <blazing-star@hotmail.de>
2017-05-06 22:20:09 +0200

    Update statistics

--- src/app/components/Statistics/Statistics.js
+++ src/app/components/Statistics/Statistics.js
@@ -8,7 +8,6 @@ import { Layout, Row, Col, Card, Button } from 'antd';
 import styles from './Statistics.scss';
 
 export default class Statistics extends React.Component {
-
 	constructor() {
 		super();
 
@@ -22,27 +21,27 @@ export default class Statistics extends React.Component {
 
 		let systems = [];
 
-		systemsRef.once('value', snap => {
-			let obj = snap.val();
+		systemsRef
+			.once('value', snap => {
+				let obj = snap.val();
 
-			for(let system in obj) {
-				const value = obj[system];
-				systems.push({system, value});
-			}
-		}).then( () => {
-			this.setState({
-				data: systems,
+				for (let system in obj) {
+					const value = obj[system];
+					systems.push({ system, value });
+				}
+			})
+			.then(() => {
+				this.setState({
+					data: systems,
+				});
 			});
-		});
-
 	}
 
 	render() {
-
 		let data = this.state.data;
 		let sortedData = [];
 		if (data !== null) {
-			sortedData = data.sort( (x, y) => y.value.games - x.value.games);
+			sortedData = data.sort((x, y) => y.value.games - x.value.games);
 		}
 
 		return (
@@ -52,28 +51,26 @@ export default class Statistics extends React.Component {
 						<h1>STATISTICS</h1>
 					</Col>
 				</Row>
-				<Row type="flex" justify="start">
-					{
-						sortedData.map( (node, i) => {
-							return(
-								<Col span="6" key={ i } className={styles.cardWrapper}>
-									<Card title={node.value.title} style={{ width: '80%' }}>
-										<CountUp start={0} end={ node.value.games } duration={3.0}/>
-									</Card>
-								</Col>
-							);
-						})
-					}
+				<Row type="flex" justify="start" className={styles.cardBox}>
+					{sortedData.map((node, i) => {
+						return (
+							<Col span="6" key={i} className={styles.cardWrapper}>
+								<Card title={node.value.title} style={{ width: '80%' }}>
+									<CountUp start={0} end={node.value.games} duration={3.0} />
+								</Card>
+							</Col>
+						);
+					})}
 				</Row>
-				<div className={ styles.fixedBar }>
+				<div className={styles.fixedBar}>
 					<Row type="flex" justify="start">
-						<Col span="2"/>
+						<Col span="2" />
 						<Col span="20">
-							<Link to='/'>
-								<Button>Back</Button>
+							<Link to="/">
+								<Button type="primary">Back</Button>
 							</Link>
 						</Col>
-						<Col span="2"/>
+						<Col span="2" />
 					</Row>
 				</div>
 			</Layout>
