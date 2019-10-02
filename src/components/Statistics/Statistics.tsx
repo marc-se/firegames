0d7880a718f2918c952ab0d2e81d939bd1054d10
Marcel Sebald <blazing-star@hotmail.de>
2019-10-02 15:22:06 +0200

    Statistics to hooks

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import CountUp from "react-countup";
 import firebase from "firebase/app";
@@ -14,22 +14,10 @@ interface StatObj {
 	value: System;
 }
 
-interface Props {}
+const Statistics = () => {
+	const [systems, setSystems] = useState([] as Array<StatObj>);
 
-interface State {
-	systems: Array<StatObj>;
-}
-
-export default class Statistics extends Component<Props, State> {
-	state = {
-		systems: []
-	};
-
-	componentDidMount() {
-		this.getSystems();
-	}
-
-	getSystems = async () => {
+	async function fetchSystems() {
 		let systems: Array<StatObj> = [];
 		try {
 			const systemsRef = await firebase.database().ref("systems");
@@ -39,53 +27,54 @@ export default class Statistics extends Component<Props, State> {
 				const value = obj[system];
 				systems.push({ system, value });
 			}
-			this.setState({
-				systems
-			});
+			setSystems(systems);
 		} catch (error) {
 			console.error(error);
 		}
-	};
-
-	render() {
-		const { systems } = this.state;
-		let sortedData: Array<StatObj> = [];
-		if (systems.length > 0) {
-			// @ts-ignore
-			sortedData = systems.sort((x, y) => y.value.games - x.value.games);
-		}
-
-		return (
-			<SC.Container>
-				<SC.GlobalStyle />
-				<SC.Head>
-					<Col span={24}>
-						<SC.LargeHeading>STATISTICS</SC.LargeHeading>
-					</Col>
-				</SC.Head>
-				<SC.CardContainer>
-					{sortedData.map((node, i) => {
-						return (
-							<SC.CardHolder span={6} key={i}>
-								<Card title={node.value.title} style={{ width: "80%" }}>
-									<CountUp start={0} end={node.value.games} duration={3.0} />
-								</Card>
-							</SC.CardHolder>
-						);
-					})}
-				</SC.CardContainer>
-				<SC.Bar>
-					<Row type="flex" justify="start">
-						<Col span={2} />
-						<Col span={20}>
-							<Link to="/">
-								<Button type="primary">Back</Button>
-							</Link>
-						</Col>
-						<Col span={2} />
-					</Row>
-				</SC.Bar>
-			</SC.Container>
-		);
 	}
-}
+
+	useEffect(() => {
+		fetchSystems();
+	}, []);
+
+	let sortedData: Array<StatObj> = [];
+	if (systems.length > 0) {
+		// @ts-ignore
+		sortedData = systems.sort((x, y) => y.value.games - x.value.games);
+	}
+
+	return (
+		<SC.Container>
+			<SC.GlobalStyle />
+			<SC.Head>
+				<Col span={24}>
+					<SC.LargeHeading>STATISTICS</SC.LargeHeading>
+				</Col>
+			</SC.Head>
+			<SC.CardContainer>
+				{sortedData.map((node, i) => {
+					return (
+						<SC.CardHolder span={6} key={i}>
+							<Card title={node.value.title} style={{ width: "80%" }}>
+								<CountUp start={0} end={node.value.games} duration={3.0} />
+							</Card>
+						</SC.CardHolder>
+					);
+				})}
+			</SC.CardContainer>
+			<SC.Bar>
+				<Row type="flex" justify="start">
+					<Col span={2} />
+					<Col span={20}>
+						<Link to="/">
+							<Button type="primary">Back</Button>
+						</Link>
+					</Col>
+					<Col span={2} />
+				</Row>
+			</SC.Bar>
+		</SC.Container>
+	);
+};
+
+export default Statistics;
