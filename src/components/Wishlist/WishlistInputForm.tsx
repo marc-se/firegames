ff78bd20bb95098894ad4d9f313d5a6a7ce822b8
Marcel Sebald <blazing-star@hotmail.de>
2019-10-13 18:04:48 +0200

    WIP: wishlist feature

--- src/components/Wishlist/WishlistInputForm.tsx
+++ src/components/Wishlist/WishlistInputForm.tsx
@@ -1,19 +1,26 @@
 import React, { ChangeEvent, useState, FormEvent } from "react";
-import { Form, Button, message } from "antd";
+import { Form, Button, Radio, message } from "antd";
 import firebase from "firebase/app";
 import "firebase/database";
 
 import SystemSelect from "../SystemSelect/SystemSelect";
 
 import { ContentWrapper, TextInput } from "./StyledComponents";
+import { RadioChangeEvent } from "antd/lib/radio";
+
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
 
 const WishlistInputForm = () => {
 	const [text, setText] = useState("");
 	const [system, setSystem] = useState("select a system");
+	const [region, setRegion] = useState("PAL");
 
 	const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
 	const handleSystemChange = (system: string) => setSystem(system);
+
+	const handleRegionChange = (e: RadioChangeEvent) => setRegion(e.target.value);
 
 	const successMessage = () => message.success("Game added to your wishlist", 3);
 
@@ -27,7 +34,8 @@ const WishlistInputForm = () => {
 				const node = {
 					title: text,
 					system: system,
-					purchased: false
+					purchased: false,
+					region: region
 				};
 				await addNodeAt.push(node);
 				successMessage();
@@ -54,6 +62,13 @@ const WishlistInputForm = () => {
 				</Form.Item>
 				<Form.Item>
 					<SystemSelect minWidth={250} handleChange={handleSystemChange} />
+				</Form.Item>
+				<Form.Item>
+					<RadioGroup onChange={handleRegionChange} defaultValue={region}>
+						<RadioButton value="PAL">PAL</RadioButton>
+						<RadioButton value="JAP">JAP</RadioButton>
+						<RadioButton value="US">US</RadioButton>
+					</RadioGroup>
 				</Form.Item>
 				<Form.Item>
 					<Button type="primary" htmlType="submit">
